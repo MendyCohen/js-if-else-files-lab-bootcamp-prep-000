454c66cf7fdb437fe74270de4e072ef97a6f044e
Mendy <mendybeltcohen@gmail.com>
2018-04-13 19:05:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (name === "susan") {
   console.log("got the name right")
 } else {
   console.log("thought the nasme was susan")
-
+}
 
 // Don't worry about this
 module.exports = { name, height
