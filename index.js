cc93d6b6d8d133b64128b22d1ba9f88ac0de1641
Mendy <mendybeltcohen@gmail.com>
2018-04-13 19:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var name = "Joe"
 var height = "74"
+if (name === "susan") {
+  console.log("got the name right")
+} else {
+  console.log("thought the nasme was susan")
 
 
 // Don't worry about this
